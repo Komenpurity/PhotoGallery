3006b93c2d8d04a3c4fa84af33b5b407854fabb9
Purity Jepkorir <jepkorirpurity8@gmail.com>
2024-02-07 10:56:22 +0300

    added users,photos,albums routes and index controller actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,15 @@
 class PhotosController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+      def index
+        photos = Photo.all
+        render json: photos
+      end
+
+
+    private
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end

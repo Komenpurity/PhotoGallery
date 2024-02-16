d88def539c739f1072bbf191171d8f73e90b6659
Purity Jepkorir <jepkorirpurity8@gmail.com>
2024-02-16 19:51:40 +0300

    changed database to postgresql and added config for production

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_07_073450) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string "album_id"
     t.string "user_id"
